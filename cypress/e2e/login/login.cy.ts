e915e7d7518ef9fcaca37de891269a2f9c26f975
AndLacerda992 <anderson.lacerda992@gmail.com>
2024-01-10 23:36:42 -0300

    comit2

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -16,6 +16,19 @@ describe('Teste login', () => {
         cy.get('#user-name').type('standard_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
-    /* ==== End Cypress Studio ==== */
+        cy.get(':nth-child(1) > .pricebar > .btn_primary').click();
+        cy.get(':nth-child(2) > .pricebar > .btn_primary').click();
+        cy.get(':nth-child(3) > .pricebar > .btn_primary').click();
+        cy.get(':nth-child(4) > .pricebar > .btn_primary').click();
+        cy.get('.svg-inline--fa').click();
+        cy.get(':nth-child(6) > .cart_item_label > .item_pricebar > .btn_secondary').click();
+        cy.get(':nth-child(5) > .cart_item_label > .item_pricebar > .btn_secondary').click();
+        cy.get('.btn_action').click();
+        cy.get('[data-test="firstName"]').type('Anderson ');
+        cy.get('[data-test="lastName"]').type('Lacerda');
+        cy.get('[data-test="postalCode"]').type('02464090');
+        cy.get('.btn_primary').click();
+        cy.get('.btn_action').click();
+        /* ==== End Cypress Studio ==== */
     });
 })
